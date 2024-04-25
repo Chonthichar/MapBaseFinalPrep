3320479dc7077a41bc95f9df07728ae24fd596f0
jofa016 <joanne1307@hotmail.com>
2024-04-25 15:09:48 +0200

    "Basic" Kommuner & Fylker

---
+++ src/modules/application/application.tsx
@@ -0,0 +1,59 @@
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
+import TileLayer from "ol/layer/Tile";
+import { OSM } from "ol/source";
+
+import "./application.css";
+import "ol/ol.css";
+import { KommuneLayerCheckbox } from "../kommune/kommuneLayerCheckbox";
+import { map, MapContext } from "../map/mapContext";
+import { Layer } from "ol/layer";
+import { KommuneAside } from "../kommune/kommuneAside";
+import { FylkeLayerCheckbox } from "../fylke/fylkeLayerCheckbox";
+import { FylkeAside } from "../fylke/fylkeAside";
+
+export function Application() {
+  function handleFocusUser(e: React.MouseEvent) {
+    e.preventDefault();
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const { latitude, longitude } = pos.coords;
+      map.getView().animate({
+        center: [longitude, latitude],
+        zoom: 12,
+      });
+    });
+  }
+
+  const [layers, setLayers] = useState<Layer[]>([
+    new TileLayer({ source: new OSM() }),
+  ]);
+  useEffect(() => map.setLayers(layers), [layers]);
+
+
+  const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+
+
+  useEffect(() => map.setTarget(mapRef.current), []);
+  return (
+      <MapContext.Provider value={{ map, layers, setLayers }}>
+        <div className="container">
+          <header className={"header"}>
+            <h1>
+              Map Prep
+            </h1>
+          </header>
+        </div>
+      <nav className="container">
+        <a href={"#"} onClick={handleFocusUser}>
+          Focus on me
+        </a>
+        <KommuneLayerCheckbox />
+        <FylkeLayerCheckbox />
+      </nav>
+      <main>
+        <div ref={mapRef}></div>
+        <FylkeAside />
+        <KommuneAside />
+      </main>
+    </MapContext.Provider>
+  );
+}
